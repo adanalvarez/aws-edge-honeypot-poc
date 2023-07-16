b808391e30dcf098425b30af45630b0ac2932891
Adan Alvarez <adan.alvarez.90@gmail.com>
2023-07-16 14:34:23 +0200

    add cloudfront_url output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "ip_set_id" {
   description = "The ID of the IP Set"
   value       = aws_wafv2_ip_set.honeypot_ip_blocklist.id
 }
+
+output "cloudfront_url" {
+  description = "The URL of the CloudFront distribution"
+  value       = "https://${aws_cloudfront_distribution.honeypot_distribution.domain_name}"
+}
+
